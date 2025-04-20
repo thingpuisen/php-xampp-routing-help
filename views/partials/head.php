8daa6df48d6c10c044728dbf3678fdb13296d9af
thingpuisen <129593701+thingpuisen@users.noreply.github.com>
2025-04-20 13:14:04 -0700

    Add files via upload

---
+++ views/partials/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+  <link rel="stylesheet" href="css/active.css ">
+  <style>
+     :root{
+        font-size: 14px;
+        --font-size: 0.875rem;
+        --spacing: 0.75rem;
+        --form-element-spacing-vertical: 0.5rem;
+        --button-padding: 0.5rem 1rem;
+        --block-spacing-vertical: 1rem;
+    }
+  </style>
+</head>
+<body>
---
+++ views/partials/nav.php
@@ -0,0 +1,22 @@
+<header class="container">
+
+    <h1 style="text-align: center;">Mizoram University Student Portal</h1>
+   
+    </style>
+  </header>
+
+  <nav class="container">
+    <ul>
+      
+      <li><a href="/index">Profile</a></li>
+      <li><a href="/attendance">Attendance</a></li>
+      <li><a href="/semester-result">Semester Results</a></li>
+      <li><a href= "/test-schedule">Test Schedule</a></li>
+      <li><a href="/exam-schedule">Exam schedule</a></li>
+      
+    </ul>
+    <ul>
+        <li><a href="/student-login">Logout</a></li>
+      </ul>
+  </nav>
+  
